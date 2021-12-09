76c10b7822a85ce08254210e9f61988b2e7c0f0d
Muhammad Nur Imansyah <9482938+muhammadnurimansyah@users.noreply.github.com>
2021-12-09 22:13:11 +0700

    Update NexObject.h

--- NexObject.h
+++ NexObject.h
@@ -17,8 +17,8 @@
 #define __NEXOBJECT_H__
 #include <Arduino.h>
 #include <stdlib.h>
-extern "C" char* utoa(int a, char* buffer, unsigned char radix);
-extern "C" char* ltoa(int a, char* buffer, unsigned char radix);
+// extern "C" char* utoa(int a, char* buffer, unsigned char radix);
+// extern "C" char* ltoa(int a, char* buffer, unsigned char radix);
 #include "NexConfig.h"
 /**
  * @addtogroup CoreAPI 
